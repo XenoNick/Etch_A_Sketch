252da58f1f07da062eda63ba71aabeba05c79cce
Nick <124405252+XenoNick@users.noreply.github.com>
2023-03-11 15:27:18 -0500

    Add logic to allow user to select grid size

--- app.js
+++ app.js
@@ -2,7 +2,17 @@ const drawingBoard = document.querySelector('.drawingBoard')
 const sizeButtons = [...document.querySelectorAll('input[type="radio"]')]
 const currentGridSize = [...document.querySelectorAll('.gridSize')]
 
+
+
+function emptyGrid() {
+    const gridSqaures = [...drawingBoard.childNodes]
+    for (let square of gridSqaures) {
+        square.remove()
+    }
+}
+
 function createGrid(width, height, size) {
+    if (drawingBoard.childElementCount > 0) emptyGrid()
     for (let i = 0; i < (width * height); i++) {
         const square = document.createElement('div')
         square.classList.add('square')
@@ -11,5 +21,31 @@ function createGrid(width, height, size) {
     }
 }
 
-createGrid(+currentGridSize[0].textContent, +currentGridSize[1].textContent,
-    sizeButtons[1].value)
+function getGridSize(e) {
+    const size = []
+    if (e.target.value === 'large') {
+        size.push(currentGridSize[0].textContent = '80')
+        size.push(currentGridSize[1].textContent = '40')
+    } else if (e.target.value === 'medium') {
+        size.push(currentGridSize[0].textContent = '32')
+        size.push(currentGridSize[1].textContent = '16')
+    } else {
+        size.push(currentGridSize[0].textContent = '16')
+        size.push(currentGridSize[1].textContent = '8')
+    }
+    return size
+}
+
+for (let sizeButton of sizeButtons) {
+    sizeButton.addEventListener('click', (e) => {
+        const size = getGridSize(e)
+        createGrid(+size[0], +size[1], e.target.value)
+    })
+}
+
+window.addEventListener('load', () => {
+    sizeButtons[1].checked = true
+    createGrid(+currentGridSize[0].textContent, +currentGridSize[1].textContent,
+        sizeButtons[1].value)
+        
+})
